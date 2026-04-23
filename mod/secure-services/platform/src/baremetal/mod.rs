d9c8c2c9e53f9492f2c83fd2bc735387af3f81d3
Phil Weber <philweber@microsoft.com>
2026-04-23 16:24:24 -0700

    Include missing files

---
+++ mod/secure-services/platform/src/baremetal/mod.rs
@@ -0,0 +1,14 @@
+mod interrupt;
+mod panic;
+pub mod services;
+pub mod uart;
+
+use aarch64_rt::entry;
+use ec_service_lib::SpLogger;
+
+entry!(aarch64_rt_main);
+fn aarch64_rt_main(_arg0: u64, _arg1: u64, _arg2: u64, _arg3: u64) -> ! {
+    log::set_logger(&SpLogger).unwrap();
+    log::set_max_level(log::LevelFilter::Off);
+    crate::main();
+}
