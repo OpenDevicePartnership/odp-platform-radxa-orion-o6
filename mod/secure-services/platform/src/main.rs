bb3336508501819163aeb547093c0d3d75c1c62d
philgweber <philweber@microsoft.com>
2026-05-01 08:29:00 -0700

    Added odp-secure-services (#50)
    * Added odp-secure-services
    
    * Include missing files

---
+++ mod/secure-services/platform/src/main.rs
@@ -0,0 +1,43 @@
+// This project is dual-licensed under Apache 2.0 and MIT terms.
+// See LICENSE-APACHE and LICENSE-MIT for details.
+
+#![cfg_attr(target_os = "none", no_std)]
+#![cfg_attr(target_os = "none", no_main)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+#![deny(unsafe_op_in_unsafe_fn)]
+
+#[cfg(target_os = "none")]
+mod baremetal;
+
+#[cfg(not(target_os = "none"))]
+fn main() {
+    println!("qemu-sp stub");
+}
+
+#[cfg(target_os = "none")]
+fn main() -> ! {
+    use ec_service_lib::MessageHandler;
+    use odp_ffa::Function;
+
+    log::info!("QEMU Secure Partition - build time: {}", env!("BUILD_TIME"));
+
+    let version = odp_ffa::Version::new().exec().unwrap();
+    log::info!("FFA version: {}.{}", version.major(), version.minor());
+
+    let battery = baremetal::services::battery::Battery::new(baremetal::uart::EcUart);
+    let thermal = baremetal::services::thermal::Thermal::new(baremetal::uart::EcUart);
+    let time_alarm = baremetal::services::time_alarm::TimeAlarm::new(baremetal::uart::EcUart);
+
+    log::info!("Running Version: 1");
+
+    MessageHandler::new()
+        .append(battery)
+        .append(thermal)
+        .append(time_alarm)
+        .append(ec_service_lib::services::FwMgmt::new())
+        .append(ec_service_lib::services::Notify::new())
+        .run_message_loop()
+        .expect("Error in run_message_loop");
+
+    unreachable!()
+}
